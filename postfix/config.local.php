66c4ca64f601b1a88f2f75e8fac11d7766c8b38a
ok_fish <306863208@qq.com>
2024-10-14 17:17:06 +0800

    postfix配置
    Signed-off-by: ok_fish <306863208@qq.com>

---
+++ postfix/config.local.php
@@ -0,0 +1,9 @@
+<?php 
+$CONF['configured'] = true;
+$CONF['database_type'] = 'mysqli';
+$CONF['database_user'] = 'postfix';
+$CONF['database_host'] = '127.0.0.1';
+$CONF['database_password'] = 'postfix';
+$CONF['database_name'] = 'postfix';
+$CONF['setup_password'] = '$2y$10$Om00Hb7o4hgG/vo9CjLknuiBn07ZqotiIAmBnl0etYLPYFLCB/qFa';
+$CONF["admin_email"] = 'root@admin.xiaoming.work';
